64e1355a74b34b0904a7a45952bdb07d245d35f0
AbdulrahmanAlotaibi <a.alotaibi.dev@gmail.com>
2022-08-27 15:36:51 +0300

    playing with bazel!

---
+++ src/IntMultiplier.java
@@ -0,0 +1,15 @@
+public class IntMultiplier {
+
+	private int a;
+	private int b;
+
+
+	public IntMultiplier(int a, int b ){
+		this.a = a;
+		this.b = b;
+	}
+
+	public int GetProduct() {
+		return a * b ;
+	}
+}
